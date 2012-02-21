0a822b707045d3535539954b0927dfe43f8a20ae
Taras Kovalchuk <7realm@gmail.com>
2012-02-21 19:15:40 +0200

    Removed deprecated XmlBeanFactory and now using ApplicationContext. However this requires more dependencies.

--- server/src/my/chat/server/Server.java
+++ server/src/my/chat/server/Server.java
@@ -9,36 +9,30 @@ import my.chat.parser.ParserService;
 import my.chat.security.SecurityService;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Server {
     private final NetworkService networkService;
     private final SecurityService securityService;
     private final DatabaseService databaseService;
 
-
     public Server() throws ChatException {
         try {
-            // init services
-            Resource resource = new FileSystemResource(FilePaths.SERVICES_CONFIG);
-            BeanFactory factory = new XmlBeanFactory(resource);
-
-            databaseService = (DatabaseService) factory.getBean("databaseService");
-
-            // databaseService = DatabaseServiceFactory.getInstance();
+            // init application context
+            ApplicationContext appContext = new FileSystemXmlApplicationContext(FilePaths.SERVICES_CONFIG);
+            
+            // create database service
+            databaseService = (DatabaseService) appContext.getBean("databaseService");
 
             securityService = new SecurityService(databaseService);
 
             networkService = NetworkService.getInstance();
             networkService.setOnConnectionListener(securityService);
-
         } catch (ClassCastException e) {
             throw new ChatException("Failed to cast created service.", e);
         } catch (BeansException e) {
-            throw new ChatException("Failed to create services.", e);
+            throw new ChatException("Failed to create service beans.", e);
         }
     }
 
